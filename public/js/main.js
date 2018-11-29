304876b7a1563434b4d37be708eb0b79c276edb2
Nick Floyd <nickfloyd@invisionapp.com>
2018-11-29 07:46:20 -0600

    Refactoring and adding timer class to queue time events for animation.

--- public/js/main.js
+++ public/js/main.js
@@ -1,17 +1,13 @@
 import { loadLevel } from './loaders.js';
 import { loadBackgroundSprites } from './sprites.js';
+import Timer from './timer.js';
 import Compositor from './compositor.js';
-import { createBackgroundLayer } from './layers.js';
+import { createSpriteLayer, createBackgroundLayer } from './layers.js';
 import { createMario } from './entities.js';
+import Keyboard from './keyboardState.js'
 
 const canvas = document.getElementById('screen');
 const context = canvas.getContext('2d');
-
-function createSpriteLayer(entity){
-  return function drawSpriteLayer(context) {
-    entity.draw(context);
-  }
-}
 
 Promise.all([
   createMario(),
@@ -24,19 +20,34 @@ Promise.all([
   const backgroundLayer = createBackgroundLayer(level.backgrounds, backgroundSprites);
   comp.layers.push(backgroundLayer);
 
-  const gravity = 0.5;
+  const gravity = 700;
+  mario.pos.set(32, 220);
 
+  const SPACE = 32;
+  const input = new Keyboard();
+  input.addMapping(SPACE, keyState => {
+    if(keyState) {
+      mario.jump.start();
+    } else {
+      mario.jump.cancel();
+    }
+  });
+  input.listenTo(window);
+  
 
   const spriteLayer = createSpriteLayer(mario);
   comp.layers.push(spriteLayer);
 
-  function update(){
+  const timer = new Timer(1/60);
+
+  timer.update = function update(deltaTime) {
+    mario.update(deltaTime);
+
     comp.draw(context);
-    mario.update();
-    mario.vel.y += gravity;
-    requestAnimationFrame(update);
+
+    mario.vel.y += gravity * deltaTime;
   }
-  update();
+  timer.start();
 });
 
 
